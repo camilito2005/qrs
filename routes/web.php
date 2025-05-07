3d93244bf3b189d601ac8297acbdd2de28e37dc7
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-05-07 14:11:02 -0500

    feat: agregue las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QRController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Formulario' , [QRController::class, 'index'])->name('Formulario');
+Route::post('/qr/Generar' , [QRController::class, 'Generar'])->name('qr.Generar');
+// Route::post('/qr/descargar' , [QRController::class, 'Download'])->name('qr.descargar');
+Route::match(['get', 'post'] , '/qr/descargar' , [QRController::class, 'Download'])->name('qr.descargar');
+
