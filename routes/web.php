7f88db76d657af3d571f1a68c30a9d2f1c27a58f
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-05-07 14:42:15 -0500

    fix: acomode el inicio

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QRController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/Formulario' , [QRController::class, 'index'])->name('Formulario');
-Route::post('/qr/Generar' , [QRController::class, 'Generar'])->name('qr.Generar');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/' , [QRController::class, 'index'])->name('Index');
+Route::get('/QR' , [QRController::class, 'index'])->name('Formulario');
+Route::post('/QR/Generar' , [QRController::class, 'Generar'])->name('qr.Generar');
 // Route::post('/qr/descargar' , [QRController::class, 'Download'])->name('qr.descargar');
 Route::match(['get', 'post'] , '/qr/descargar' , [QRController::class, 'Download'])->name('qr.descargar');
 
